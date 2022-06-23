00f1a8b73c2ff47d4c7930d84ddf1e5440fd20b0
Elious Mondal <66035003+EliousMondal@users.noreply.github.com>
2022-06-23 12:41:37 -0400

    KSign function added to determine the sign of k-vector of the incident radiation

--- feynDiag.py
+++ feynDiag.py
@@ -11,8 +11,16 @@ def numDiags():
     return np.array(diagrams)
 
 def KSide(prevEl,currEl):
-    """returns positive value if right side and negative value if left side"""
+    """returns +1 if bra side and -1 if ket side"""
     ketDiff = currEl[0]-prevEl[0]
     braDiff = currEl[1]-prevEl[1]
     relDiff = np.abs(braDiff)-np.abs(ketDiff)
-    return np.sign(relDiff)
+    return relDiff//np.abs(relDiff)
+
+def KSign(prevEl,currEl):
+    """returns +1 if +k and -1 if -k"""
+    kside = KSide(prevEl,currEl)
+    ketDiff = prevEl[0]-currEl[0]
+    braDiff = prevEl[1]-currEl[1]
+    netDiff = ketDiff+braDiff
+    return (kside*netDiff)//np.abs(kside*netDiff)
