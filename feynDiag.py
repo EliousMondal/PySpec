ca85a3b247be235685348a3dc3815c1e9a4cf8b5
Elious Mondal <66035003+EliousMondal@users.noreply.github.com>
2022-06-23 14:55:41 -0400

    sigCombo generates combinations of lasers with different k-vectors

--- feynDiag.py
+++ feynDiag.py
@@ -1,26 +1,29 @@
 import numpy as np
 
-def numDiags():
-    """generates the possible diagrams for 2D process"""
-    signs = np.array([1,-1])
-    diagrams = []
-    for i in signs:
-        for j in signs:
-            for k in signs:
-                diagrams.append(np.array([i,j,k]))
-    return np.array(diagrams)
 
 def KSide(prevEl,currEl):
-    """returns +1 if bra side and -1 if ket side"""
+    """returns +1 if bra side and -1 if ket side
+       Need to be rigoruosly studied for larger hilbert spaces"""
     ketDiff = currEl[0]-prevEl[0]
     braDiff = currEl[1]-prevEl[1]
     relDiff = np.abs(braDiff)-np.abs(ketDiff)
     return relDiff//np.abs(relDiff)
 
 def KSign(prevEl,currEl):
-    """returns +1 if +k and -1 if -k"""
+    """returns +1 if +k and -1 if -k
+       Need to be rigoruosly studied for larger hilbert spaces"""
     kside = KSide(prevEl,currEl)
     ketDiff = prevEl[0]-currEl[0]
     braDiff = prevEl[1]-currEl[1]
     netDiff = ketDiff+braDiff
     return (kside*netDiff)//np.abs(kside*netDiff)
+
+def sigCombo():
+    """possible combination of signals"""
+    signs = np.array([1,-1])
+    sigComb = []
+    for i in signs:
+        for j in signs:
+            for k in signs:
+                sigComb.append(np.array([i,j,k]))
+    return np.array(sigComb)
