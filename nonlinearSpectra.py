b5796e974af349052598d8d87955cb8912cab005
Elious Mondal <66035003+EliousMondal@users.noreply.github.com>
2022-06-25 10:36:54 -0400

    initialisation of feynman diagrams

--- nonlinearSpectra.py
+++ nonlinearSpectra.py
@@ -6,21 +6,32 @@ import specFunctions as sF
 μ_t0 = model.μ()
 ρ_t0 = model.ρ0()
 μx_t0 = sF.commutator(μ_t0,ρ_t0)
-n0 = sF.non0(μx_t0)
+n0 = sF.non0(μx_t0)                         # Non-zero indices of first [μ,ρ]
+ρlen = ρ_t0.shape[0]*ρ_t0.shape[1]
 
 def simulate(itraj,TrajFolder):
-
-    """Initialising response matrix"""
-    R3 = np.zeros(model.NSteps1, model.NSteps2, model.NSteps3) 
 
     """first loop over the non zero elements in [μ,ρ0]"""
     for ij in range(n0.shape[1]):
 
-        """Forward and backward focused states for first PLDM run"""
+        """Initialising response and weight matrix"""
+        R3 = np.zeros(model.NSteps1, model.NSteps2, model.NSteps3, dtype=complex)          # Response array
+        wt = np.zeros(model.NSteps1, model.NSteps2, model.NSteps3, dtype=complex)          # weight for each response 
+        wt[:,0,0] = np.ones(wt.shape[0])*μx_t0[iF0,iB0]                                    # initial weights according to μx_t0 elements
+
+        """Keeping track of feynman diagrams"""
+        kside = np.zeros(model.NSteps1, model.NSteps2, model.NSteps3, dtype=int)
+        ksign = np.zeros(model.NSteps1, model.NSteps2, model.NSteps3, dtype=int)
+
+        """first PLDM run for first laser pulse"""
         iF0, iB0 = n0[ij][0], n0[ij][1]
         iBath = np.loadtxt(TrajFolder + f"{itraj+1}/initial_bath_{itraj+1}.txt")
         iR, iP = iBath[:,0], iBath[:,1]
-        R3[:,0,0] = method.runTraj(iR, iP, iF0, iB0, itraj, model.NSteps1)
+        ρijt1 = method.runTraj(iR, iP, iF0, iB0, itraj, model.NSteps1)
+
+
+
+
 
 
 
