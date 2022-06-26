c8e21acd3ae5b9d81a02aa6d56c172f1bf2ef09f
Elious Mondal <66035003+EliousMondal@users.noreply.github.com>
2022-06-26 15:03:28 -0400

    2nd laser pulse PLDM simulation added

--- nonlinearSpectra.py
+++ nonlinearSpectra.py
@@ -36,21 +36,31 @@ def simulate(itraj,TrajFolder):
         wt[ij,:,:] = np.ones(wt.shape[0])*μx_t0[iF0,iB0]                                   # initial weights according to μx_t0 elements
         iBath = np.loadtxt(TrajFolder + f"{itraj+1}/initial_bath_{itraj+1}.txt")
         iR, iP = iBath[:,0], iBath[:,1]
-        ρijT1 = method.runTraj(iR, iP, iF0, iB0, itraj, model.NSteps1)
+        ρij_t1, r_t1, p_t1 = method.runTraj(iR, iP, iF0, iB0, itraj, model.NSteps1)
 
         for t1Index in range(model.NSteps1):                                               # Can be parallelised at this level
-            """"For each t in 0->t1, do PLDM t->t2 by focusing ρ at t"""
 
             """extracting ρ(t1Index) from ρijT1"""
-            ρT1Re = ρijT1[t1Index,1:(ρlen+1)].reshape(ρ_t0.shape[0],ρ_t0.shape[1])
-            ρT1Im = ρijT1[t1Index,(ρlen+1):].reshape(ρ_t0.shape[0],ρ_t0.shape[1])
-            ρT1 = ρT1Re + 1j*ρT1Im
+            ρ_t1Re = ρij_t1[t1Index,1:(ρlen+1)].reshape(ρ_t0.shape[0],ρ_t0.shape[1])
+            ρ_t1Im = ρij_t1[t1Index,(ρlen+1):].reshape(ρ_t0.shape[0],ρ_t0.shape[1])
+            ρ_t1 = ρ_t1Re + 1j*ρ_t1Im
 
             """Focusing to get the iF and iB for next PLDM and traj weight of current trajectory run"""
-            focusedElT1, trajWeight = foc.focus(ρT1)
-            iFT1, iBT1 = focusedElT1
-            wt[ij,t1Index,:] = wt[ij,:,:]*trajWeight  
+            μx_t1 = sF.commutator(μ_t0,ρ_t1)
+            focusedEl_t1, trajWeight = foc.focus(μx_t1)
+            iF_t1, iB_t1 = focusedEl_t1
+            wt[ij,t1Index,:] = trajWeight*μx_t1[iF_t1,iB_t1]  
 
             """2nd light-matter perturbation interation in Feynman diagram"""
-            kside[ij,t1Index,:] = fD.KSide(n0[ij],focusedElT1)
-            ksign[ij,t1Index,:] = fD.KSign(n0[ij],focusedElT1)
+            kside[ij,t1Index,:] = fD.KSide(n0[ij],focusedEl_t1)
+            ksign[ij,t1Index,:] = fD.KSign(n0[ij],focusedEl_t1)
+
+            """position and momentum of bath at t∈(0,t1)"""
+            iR_t1, iP_t1 = r_t1[t1Index,:], p_t1[t1Index,:]
+
+            """"For each t∈[0,t1], running PLDM t->t2 by focusing ρ at t"""
+            ρij_t2, r_t2, p_t2 = method.runTraj(iR_t1, iP_t1, iF_t1, iB_t1, itraj, model.NSteps2)
+
+
+
+
