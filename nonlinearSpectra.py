corrected array shapes and weighting function progressions

nonlinearSpectra.py
@@ -8,7 +8,7 @@ import feynDiag as fD
 μ_t0 = model.μ()
 ρ_t0 = model.ρ0()
 μx_t0 = sF.commutator(μ_t0,ρ_t0)
-n0 = sF.non0(μx_t0)                         # Non-zero indices of [μ,ρ]
+n0 = sF.non0(μx_t0)                                                                                                             # Non-zero indices of [μ,ρ]
 ρlen = ρ_t0.shape[0]*ρ_t0.shape[1]
 
 def simulate(itraj,TrajFolder):
@@ -26,21 +26,21 @@ def simulate(itraj,TrajFolder):
     for ij in range(n0.shape[1]):
 
         """Initialising response and weight matrix"""
-        R3 = np.zeros((model.NSteps1, model.NSteps2, model.NSteps3), dtype=complex)          # Response array
+        R3 = np.zeros((model.NSteps1, model.NSteps2, model.NSteps3), dtype=complex)                                             # Response array
         
         """Initialising the Feynman diagrams"""
-        kside[ij,:,:] = fD.KSide(np.array([0,0]),n0[ij])                                   # side of perturbation
-        ksign[ij,:,:] = fD.KSign(np.array([0,0]),n0[ij])                                   # sign of K-vector
+        kside[ij,:,:] = np.ones((kside.shape[1],kside.shape[2]),dtype=int) * fD.KSide(np.array([0,0]),n0[ij])                   # side of perturbation
+        ksign[ij,:,:] = np.ones((ksign.shape[1],ksign.shape[2]),dtype=int) * fD.KSign(np.array([0,0]),n0[ij])                   # sign of K-vector
 
         """first PLDM run for first laser pulse"""
         iF0, iB0 = n0[ij][0], n0[ij][1]
-        wt[ij,:,:] = np.ones((wt.shape[1],wt.shape[2]))*μx_t0[iF0,iB0]                                   # initial weights according to μx_t0 elements
+        wt[ij,:,:] = np.ones((wt.shape[1],wt.shape[2]),dtype=complex) * μx_t0[iF0,iB0]                                            # initial weights according to μx_t0 elements
         iBath = np.loadtxt(TrajFolder + f"{itraj+1}/initial_bath_{itraj+1}.txt")
         iR, iP = iBath[:,0], iBath[:,1]
         ρij_t1, r_t1, p_t1 = method.runTraj(iR, iP, iF0, iB0, itraj, model.NSteps1)
 
 
-        for t1Index in range(model.NSteps1):                                               # Can be parallelised at this level
+        for t1Index in range(model.NSteps1):                                                                                    # Can be parallelised at this level
             """Second laser pulse"""
 
             """extracting ρ(t1Index) from ρij_t1"""
@@ -52,11 +52,11 @@ def simulate(itraj,TrajFolder):
             μx_t1 = sF.commutator(μ_t0,ρ_t1)
             focusedEl_t1, trajWeight_t1 = foc.focus(μx_t1)
             iF_t1, iB_t1 = focusedEl_t1
-            wt[ij,t1Index,:] = trajWeight_t1 * μx_t1[iF_t1,iB_t1]  
+            wt[ij,t1Index,:] *= np.ones(wt.shape[2],dtype=complex) * trajWeight_t1 * μx_t1[iF_t1,iB_t1]  
 
             """2nd light-matter perturbation interation in Feynman diagram"""
-            kside[ij,t1Index,:] = fD.KSide(n0[ij],focusedEl_t1)
-            ksign[ij,t1Index,:] = fD.KSign(n0[ij],focusedEl_t1)
+            kside[ij,t1Index,:] = np.ones(kside.shape[2]) * fD.KSide(n0[ij],focusedEl_t1)
+            ksign[ij,t1Index,:] = np.ones(ksign.shape[2]) * fD.KSign(n0[ij],focusedEl_t1)
 
             """position and momentum of bath at t∈[0,t1]"""
             iR_t1, iP_t1 = r_t1[t1Index,:], p_t1[t1Index,:]
@@ -77,7 +77,7 @@ def simulate(itraj,TrajFolder):
                 μx_t2 = sF.commutator(μ_t0,ρ_t2)
                 focusedEl_t2, trajWeight_t2 = foc.focus(μx_t2)
                 iF_t2, iB_t2 = focusedEl_t2
-                wt[ij,t1Index,t2Index] = trajWeight_t2 * μx_t1[iF_t2,iB_t2]
+                wt[ij,t1Index,t2Index] *= trajWeight_t2 * μx_t1[iF_t2,iB_t2]
 
                 """3rd light-matter perturbation interation in Feynman diagram"""
                 kside[ij,t1Index,t2Index] = fD.KSide(n0[ij],focusedEl_t2)
@@ -88,7 +88,7 @@ def simulate(itraj,TrajFolder):
 
                 """"For each t∈[t1,t2], running PLDM t->t3 by focusing ρ at t"""
                 ρij_t3, r_t3, p_t3 = method.runTraj(iR_t2, iP_t2, iF_t2, iB_t2, itraj, model.NSteps3)
-                wt3 = wt[ij,t1Index,t2Index]*wt[ij,t1Index,0]*wt[ij,0,0]
+                wt3 = wt[ij,t1Index,t2Index]
                 ksideVec = np.array([kside[ij,t1Index,t2Index],kside[ij,t1Index,0],kside[ij,0,0]])
 
                 for t3Index in range(model.NSteps3):
@@ -98,7 +98,7 @@ def simulate(itraj,TrajFolder):
                     ρ_t3 = ρ_t3Re + 1j*ρ_t3Im
 
                     """3rd order response calculation with all weights"""
-                    rt3 = (1j**3)*np.trace(μ_t0@(ρ_t3*wt3))*((-1)**np.sum(ksideVec))
+                    rt3 = (1j**3) * np.trace(μ_t0 @ (ρ_t3*wt3)) * ((-1)**np.sum(ksideVec))
                     R3[t1Index,t2Index,t3Index] = rt3
         
         R3_all[tuple(ij)] = [R3,ksign]